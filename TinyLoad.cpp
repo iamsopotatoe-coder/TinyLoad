cff7f50adc45ecc13e405a665c782c350a87badb
iamsopotatoe <229453216+iamsopotatoe-coder@users.noreply.github.com>
2026-05-05 20:22:34 +0200

    Update TinyLoad.cpp

--- TinyLoad.cpp
+++ TinyLoad.cpp
@@ -1,4 +1,4 @@
-// Tinyload v3, MIT license, https://github.com/iamsopotatoe-coder/TinyLoad/
+// Tinyload v3.1, MIT license, https://github.com/iamsopotatoe-coder/TinyLoad/
 #include <windows.h>
 #include <vector>
 #include <string>
